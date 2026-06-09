40184fe3de726ab783cd3a0531c107003f5bb390
SafeerCRM <crmapp1208@gmail.com>
2026-06-09 18:44:11 +0530

    Fix JWT strategy user repository injection

--- solar-crm-backend/src/auth/auth.module.ts
+++ solar-crm-backend/src/auth/auth.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { PassportModule } from '@nestjs/passport';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 import { JwtStrategy } from './jwt.strategy';
+import { User } from '../users/user.entity';
 
 @Module({
-  imports: [PassportModule],
+  imports: [
+    PassportModule,
+    TypeOrmModule.forFeature([User]),
+  ],
   providers: [JwtStrategy],
   exports: [PassportModule],
 })
